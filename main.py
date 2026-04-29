78c4fda8f149cc913a41e21eec8bb62660ee114f
pitorro123 <pipe.kmx@hotmail.com>
2026-04-29 08:04:43 -0500

    se craron si mulacion de  datos para ser limpiados

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import pandas as pd
 
 #zona para importar simulaciones
 
-from utils.simulaciónDatosSocios  import generar_socios
+from utils.simulaciónDatosSocios import generar_socios
+from utils.simulacionClases import generar_clases
 
 #zona para importar limpiezas 
 
 from notebook.limpiezaDatosSocios import limpiar_socios
+from notebook.limpiezaDatosClases import limpiar_clases
 
 
 #Creando las simulaciones
-simulaciones=generar_socios(100)
-print(simulaciones)
+simulaciones_socios=generar_socios(100)
+simulaciones_clases=generar_clases(100)
+
+print(simulaciones_socios)
+print(simulaciones_clases)
 
 #Ordenando las simulaciones
-simulaciones_ordenadas=pd.DataFrame(simulaciones)
+simulaciones_socios_ordenadas=pd.DataFrame(simulaciones_socios)
+simulaciones_clases_ordenadas=pd.DataFrame(simulaciones_clases)
 
 #limpiando el set de datos
-simulaciones_limpias=limpiar_socios(simulaciones_ordenadas)
-print(simulaciones_limpias)
+simulaciones_socios_limpias=limpiar_socios(simulaciones_socios_ordenadas)
+simulaciones_clases_limpias=limpiar_clases(simulaciones_clases_ordenadas)
+
+print(simulaciones_socios_limpias)
+print(simulaciones_clases_limpias)
